84c15f653a818521b84661a43ddc78be1d7044f3
kimyeojin <matilda94616@gmail.com>
2024-11-10 11:37:07 +0900

    feat: 내 예약조회 및 타석조회 추가

--- src/main/java/com/smart/booking/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/smart/booking/domain/reservation/repository/ReservationRepository.java
@@ -1,8 +1,13 @@
 package com.smart.booking.domain.reservation.repository;
 
+import com.smart.booking.domain.member.entity.Member;
 import com.smart.booking.domain.reservation.entity.Reservation;
+import com.smart.booking.domain.store.entity.Store;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, String> {
-
+    List<Reservation> findAllByReservationMemberMemberAndReservationDateIsAfter(Member member, LocalDate reservationDate);
+    List<Reservation> findAllByStoreAndReservationDate(Store store, LocalDate reservationDate);
 }
---
+++ src/main/java/com/smart/booking/facade/dto/reservation/ReservationSimpleResponse.java
@@ -0,0 +1,10 @@
+package com.smart.booking.facade.dto.reservation;
+
+public class ReservationSimpleResponse {
+    private String reservationNo;
+    private String reservationDate;
+    private String startTime;
+    private String endTime;
+    private String storeName;
+    private String teeBoxNumber;
+}
