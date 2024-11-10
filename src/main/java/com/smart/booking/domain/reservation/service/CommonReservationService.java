0cd2787d9063b964c094448002908b04d978f080
kimyeojin <matilda94616@gmail.com>
2024-11-10 11:37:36 +0900

    refactor: 공통로직 정리

--- src/main/java/com/smart/booking/domain/reservation/service/CommonReservationService.java
+++ src/main/java/com/smart/booking/domain/reservation/service/CommonReservationService.java
@@ -1,33 +1,11 @@
 package com.smart.booking.domain.reservation.service;
 
-import com.smart.booking.domain.reservation.dto.ReservationDetailResponse;
+import com.smart.booking.domain.reservation.entity.Reservation;
+import com.smart.booking.domain.store.entity.Store;
+import java.time.LocalDate;
+import java.util.List;
 
-public abstract class CommonReservationService {
-    /**
-     * 예약 취소
-     * @param reservationId
-     */
-    public void cancelReservation(String reservationId) {
-        if (validateCancelPermission(reservationId)) {
-            // todo 예약취소 로직
-            // admin : 관리자체크 , user: 자기 예약인지 체크
-        }
-    }
+public interface CommonReservationService {
+    List<Reservation> getReservationByStoreAndReservationDate(Store store, LocalDate reservationDate);
 
-    /**
-     * 예약 상세조회
-     * @param reservationId
-     * @return
-     */
-    public ReservationDetailResponse getReservationDetail(String reservationId) {
-        // todo 예약 조회
-        if (validateSearchPermission(reservationId)) {
-            // admin : 관리자체크 , user: 자기 예약인지 체크
-        }
-        return null;
-    }
-
-    protected abstract boolean validateCancelPermission(String reservationId);
-
-    protected abstract boolean validateSearchPermission(String reservationId);
 }
