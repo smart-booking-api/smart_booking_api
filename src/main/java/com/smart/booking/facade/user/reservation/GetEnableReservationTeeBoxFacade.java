72da023d8340f857c1f55517233e1f4b38fdf610
kimyeojin <matilda94616@gmail.com>
2024-11-12 23:50:07 +0900

    feat: 예약가능 타석 조회

--- src/main/java/com/smart/booking/facade/user/reservation/GetEnableReservationTeeBoxFacade.java
+++ src/main/java/com/smart/booking/facade/user/reservation/GetEnableReservationTeeBoxFacade.java
@@ -6,9 +6,10 @@ import com.smart.booking.domain.reservation.service.UserReservationService;
 import com.smart.booking.domain.store.entity.Store;
 import com.smart.booking.domain.store.service.StoreUserService;
 import com.smart.booking.domain.tee_box.entity.TeeBox;
-import com.smart.booking.domain.tee_box.service.TeeBoxService;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -21,11 +22,15 @@ public class GetEnableReservationTeeBoxFacade {
 
     public List<String> getEnableReservationTeeBox(String storeId, String reservationDate) throws CommonException {
         Store store = storeUserService.getStoreById(storeId);
-        List<Reservation> reservations = userReservationService.getReservationByStoreAndReservationDate(store, LocalDate.parse(reservationDate));
+        List<TeeBox> teeBox = new ArrayList<>();
+        //todo
+//      List<TeeBox> teeBox = teeBoxService.getTeeBoxByStore(store);
+        List<Reservation> reservations = userReservationService.getStoreReservation(store, LocalDate.parse(reservationDate));
         List<String> TeeBoxIds = reservations.stream()
+            .filter(reservation -> !teeBox.contains(reservation.getBox().getId()))
             .map(item-> item.getBox().getId())
-            .toList();
-//        teeBoxService.getTeeBoxNotIn(store, TeeBoxIds);
-        return null;
+            .collect(Collectors.toList());
+
+        return TeeBoxIds;
     }
 }
