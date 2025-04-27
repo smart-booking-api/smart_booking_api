4451ee02bab084af5299e4b210a9993e1e3f6a17
kimyeojin <matilda94616@gmail.com>
2025-04-27 19:14:11 +0900

    feat: pos 예약 endpoint 수정(SHB-115)

--- src/main/java/com/smart/booking/presentation/controller/endPoint/PosReservationEndpoint.java
+++ src/main/java/com/smart/booking/presentation/controller/endPoint/PosReservationEndpoint.java
@@ -6,6 +6,6 @@ public class PosReservationEndpoint {
     private static final String V1 = BASE + "/v1/reservation";
     public static final String GET_RESERVATION = V1 + "/{reservationNo}";
     public static final String START_GAME = V1 + "/game/start/{reservationNo}";
-    public static final String GET_ENABLE_RESERVATION_TIME = BASE + "/time";
-    public static final String RESERVATION_LOCK = BASE + "/lock";
+    public static final String GET_ENABLE_RESERVATION_TIME = V1 + "/time";
+    public static final String RESERVATION_LOCK = V1 + "/lock";
 }
