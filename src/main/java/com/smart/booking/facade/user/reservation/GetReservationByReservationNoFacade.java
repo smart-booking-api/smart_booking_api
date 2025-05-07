4ec8bde8d431d6022966aaf03dbaab6bfdb05357
kimyeojin <matilda94616@gmail.com>
2025-05-07 22:51:16 +0900

    refactor: 시간 조회 파사드 수정(SHB-115)

--- src/main/java/com/smart/booking/facade/user/reservation/GetReservationByReservationNoFacade.java
+++ src/main/java/com/smart/booking/facade/user/reservation/GetReservationByReservationNoFacade.java
@@ -1,6 +1,7 @@
 package com.smart.booking.facade.user.reservation;
 
 import com.smart.booking.domain.reservation.entity.Reservation;
+import com.smart.booking.domain.reservation.entity.ReservationTimeCode;
 import com.smart.booking.domain.reservation.service.ReservationTimeService;
 import com.smart.booking.domain.reservation.service.UserReservationService;
 import com.smart.booking.facade.dto.reservation.ReservationSimpleResponseDto;
@@ -16,6 +17,16 @@ public class GetReservationByReservationNoFacade {
     public ReservationSimpleResponseDto execute(int reservationNo) {
         Reservation reservation = userReservationService.getReservationByReservationNo(reservationNo);
 
-        return new ReservationSimpleResponseDto(reservation, reservationTimeService);
+        return new ReservationSimpleResponseDto(reservation, getTimeByTimeId(reservation.getStartTimeId()), getTimeByTimeId(reservation.getEndTimeId()));
+    }
+
+    /**
+     * 시간코드 조회
+     * @param timeId
+     * @return
+     */
+    private String getTimeByTimeId(String timeId) {
+        ReservationTimeCode timeCode = reservationTimeService.getReservationTimeCodeById(timeId);
+        return timeCode.getTimeName();
     }
 }
